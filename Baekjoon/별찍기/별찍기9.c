41b56e1987260489e17c3a33dd0c1e236053efa3
padoling <s_hi32@naver.com>
2018-01-30 22:06:29 +0900

    2446

---
+++ Baekjoon/별찍기/별찍기9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+  int N, i, j;
+  scanf("%d", &N);
+
+  for(i=N; i>0; i--) {
+    for(j=N; j>i; j--)
+      printf(" ");
+    for(j=0; j<i*2-1; j++)
+      printf("*");
+    printf("\n");
+  }
+  for(i=2; i<=N; i++) {
+    for(j=N; j>i; j--)
+      printf(" ");
+    for(j=0; j<i*2-1; j++)
+      printf("*");
+    printf("\n");
+  }
+  return 0;
+}
