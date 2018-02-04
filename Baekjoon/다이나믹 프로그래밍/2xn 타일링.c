5a69270959b79c2b30ac643009b0723e66f296ec
padoling <s_hi32@naver.com>
2018-02-05 01:55:18 +0900

    11726

---
+++ Baekjoon/다이나믹 프로그래밍/2xn 타일링.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int n,i;
+  int d[1001];
+  scanf("%d", &n);
+  d[0] = 1;
+  d[1] = 1;
+  for(i=2; i<=n; i++) {
+    d[i] = d[i-1] + d[i-2];
+    d[i] %= 10007;
+  }
+  printf("%d", d[n]);
+  return 0;
+}
